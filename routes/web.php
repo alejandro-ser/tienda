a9f5746c7fd94facb36a94e81f07dfffb56f87a2
Alejo <alejosegi@gmail.com>
2020-04-26 13:35:05 +0200

    Instalación y configuración de la pnatilla AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,10 @@ Route::get('/', function () {
     return view('tienda.index');
 });
 
+Route::get('/admin', function () {
+    return view('admin.category.create');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
